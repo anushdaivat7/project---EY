89ad66d2213851957a996b61567863db997096a2
anushdaivat7 <159226255+anushdaivat7@users.noreply.github.com>
2025-03-06 15:55:10 +0530

    Update Admin.jsx

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -11,7 +11,7 @@ const AdminLogin = () => {
   const [showNote, setShowNote] = useState(false); // ✅ Toggle note visibility
   const navigate = useNavigate();
 
-  const API_URL = "https://recipe-avij.onrender.com/login"; // Backend API
+  const API_URL = "https://recipe-avij.onrender.com/admin/login"; // Backend API
 
   // Handle input changes
   const handleChange = (e) => {
